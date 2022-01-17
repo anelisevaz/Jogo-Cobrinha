d02fdc89ae32ef884a0c30daa83d98a0c971a2d6
anelisevaz.anelise <anevaz1@gmail.com>
2022-01-17 10:23:51 -0300

    Iniciando

--- Game.java
+++ Game.java
@@ -2,4 +2,35 @@ import java.awt.Canvas;
 
 public class Game extends Canvas implements Runnable{
 
+  public Game(){
+    this.setPreferredSize(new Dimension(480,480));
+  }
+
+  public void tick(){
+
+  }
+
+  public void render(){
+    BufferStrategy bs = this.getBufferStrategy();
+    if(bs == null){
+      this.createBufferStrategy(3);
+      return;
+    }
+    Graphics g = bs.getDrawGraphics();
+    g.setColor(color.black);
+    g.fillRect(0, 0, 480, 480);
+
+    g.dispose();
+    bs.show();
+  }
+
+  @Override
+  public void run(){
+    while(true){
+      tick();
+      render();
+      Thread.sleep(1000/60);
+    }
+  }
+
 }
